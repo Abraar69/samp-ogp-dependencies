d5a05e3a2a5c0048bd3eb9b4076dd7f31e8af135
Abraar <86068038+Abraar69@users.noreply.github.com>
2022-01-17 20:52:49 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-apt-get update
-apt-get upgrade
+apt -y update
+apt -y upgrade
+apt -y --no-install-recommends install curl unzip lib32gcc1 ca-certificates
+apt install libstdc++6
+apt-get install lib32stdc++6
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
+sudo apt-get install libc6-i386
+sudo apt-get install lib32gcc1
 
 
 echo -e "
