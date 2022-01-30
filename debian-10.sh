c51630732d155303ff4abbae51e07227f910962b
Abraar <86068038+Abraar69@users.noreply.github.com>
2022-01-30 12:01:50 +0000

    Added Dependencies for Debian10

---
+++ debian-10.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo dpkg --add-architecture i386; sudo apt update; sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6
+
+echo -e "Dependencies has been installed for Debian 10 . . ."
