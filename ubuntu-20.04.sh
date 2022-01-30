2ba56d20d4c737735951813be405c9ef3bd0b63b
Abraar <86068038+Abraar69@users.noreply.github.com>
2022-01-30 11:59:34 +0000

    Updated Dependencies

--- ubuntu-20.04.sh
+++ ubuntu-20.04.sh
@@ -2,5 +2,6 @@
 
 sudo dpkg --add-architecture i386; sudo apt update; sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6
 
+
 echo -e "Dependencies has been installed for Ubuntu 20.04 . . ."
 
