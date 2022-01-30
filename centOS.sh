9a503ba11c0da6cdb59d0137597e657680ebacea
Abraar <86068038+Abraar69@users.noreply.github.com>
2022-01-30 12:05:37 +0000

    Added Dependencies for CentOS

---
+++ centOS.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+yum install epel-release
+yum install curl wget tar bzip2 gzip unzip python3 binutils bc jq tmux glibc.i686 libstdc++ libstdc++.i686
+
+echo -e "Dependencies has been installed for CentOS . . ."
