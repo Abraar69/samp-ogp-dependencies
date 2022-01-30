567a89926eb3d71fcaa8591d2026977ee7dc9ec4
Abraar <86068038+Abraar69@users.noreply.github.com>
2022-01-30 11:53:18 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-apt -y update
-apt -y upgrade
-apt -y --no-install-recommends install curl unzip lib32gcc1 ca-certificates
-apt install libstdc++6 #important
-apt-get install lib32stdc++6
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
-sudo apt-get install libc6-i386
-sudo apt-get install lib32gcc1
-
+sudo dpkg --add-architecture i386; sudo apt update; sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6
 
 echo -e "Dependencies has been installed . . ."
 
